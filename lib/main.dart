4a51edcebc6a01b8e52d3f299ae0958a9762bb73
gquende <gquende@hotmail.com>
2025-01-11 08:31:39 +0100

    Update iOS config

--- lib/main.dart
+++ lib/main.dart
@@ -9,4 +9,21 @@ void main() async {
   runApp(App(
     isDarkMode: false,
   ));
+
+ // runApp(TEST2());
+}
+
+
+class TEST2 extends StatefulWidget {
+  const TEST2({super.key});
+
+  @override
+  State<TEST2> createState() => _TEST2State();
+}
+
+class _TEST2State extends State<TEST2> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(appBar: AppBar(title: Text("TEST"),),),);
+  }
 }
